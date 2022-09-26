9310e5baedefccf3170eccd2d0658f52c9ef6fc2
Roberto Pauletto <roberto.pauletto@gmail.com>
2022-09-26 14:47:13 +0200

    test: add new tests for TmDBSession and TmDBTvSession

--- tmdbRest/test_tmDBSession.py
+++ tmdbRest/test_tmDBSession.py
@@ -1,27 +1,66 @@
 # test_tmDBSession
-from configparser import ConfigParser
+import os
 from unittest import TestCase
+
+from dotenv import load_dotenv
+
 import tmdbRest.tmdbapi as api
+import entities as ent
 
 
 __doc__ = """test_tmDBSession"""
-__version__ = "0.1"
-__changelog__ = """
 
-"""
 
-ini = ConfigParser()
-ini.read('config_test.py')
-API_KEY = ini.get('Misc', 'api_key')
+API_KEY = os.environ.get("TMDB_API_KEY", "")
 
 
 class TestTmDBSession(TestCase):
     def setUp(self):
-        self.session = api.TmDBSession(API_KEY)
+        self.tv_session = api.TmDBTvSession(API_KEY)
 
+    def testApiKeyFromEnvIsNotEmpty(self):
+        self.assertNotEqual(API_KEY, "")
 
+    def testSessionFactoryCallReturnTmDBTvSession(self):
+        for word in api.TVSERIES_SESSION_SYNONYM.split():
+            self.assertIsInstance(
+                api.session_factory(word, API_KEY), api.TmDBTvSession
+            )
 
-if __name__ == '__main__':
-    pass
+    def testSessionFactoryCallReturnTmDBMovieSession(self):
+        for word in api.MOVIE_SESSION_SYNONYM.split():
+            self.assertIsInstance(
+                api.session_factory(word, API_KEY), api.TmDBMovieSession
+            )
 
+    def testSessionFactoryCallNotRecognized(self):
+        self.assertRaises(
+            api.TmDBSessionException, api.session_factory, 'foo', API_KEY)
 
+    def testTVSearchShowMany(self):
+        shows = self.tv_session.search_show('grimm', False)
+        self.assertTrue(len(shows) > 0)
+
+    def testTVSearchShowExact(self):
+        shows = self.tv_session.search_show('grimm', True)
+        self.assertTrue(len(shows) == 1)
+
+    def testTVSearchShowFailed(self):
+        shows = self.tv_session.search_show('adsfasdfdsfddsfsad', True)
+        self.assertTrue(len(shows) == 0)
+
+    def testTVGetShowByID(self):
+        grimm: ent.TvShowFromSearch = self.tv_session.search_show(
+            'grimm', True
+        )[0]
+        show = self.tv_session.get_show(grimm.id)
+        self.assertEqual(show.name, 'Grimm', 'Expected "Grimm"')
+        self.assertEqual(show.status, 'Ended', "Expected 'Ended'")
+        self.assertEqual(show.tot_seasons, 6, 'Expected 6')
+
+    def testTVGetShowAndSeasonsByID(self):
+        grimm: ent.TvShowFromSearch = self.tv_session.search_show(
+            'grimm', True
+        )[0]
+        show, seasons = self.tv_session.get_show_and_seasons(grimm.id)
+        self.assertEqual(len(seasons), 6, 'Expected 6')
